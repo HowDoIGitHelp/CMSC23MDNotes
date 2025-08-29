6d8057222b4f888cb54a36f535a0e5c3ad2cac22
Rub Abella <bbadoodles@gmail.com>
2025-08-29 14:04:00 +0800

    update haskell code examples

--- Code/Haskell Code/test.hs
+++ Code/Haskell Code/test.hs
@@ -1,14 +1,31 @@
+
+
+
+
 s :: Int -> Int
 s x = x + 1
 
 p :: Int -> Int
 p x = x - 1
 
-add :: Int -> Int -> Int
-add x y = x + y
-
 applyTwice :: (Int -> Int) -> Int -> Int
 applyTwice f x = f (f x)
 
+applyThrice :: (Int -> Int) -> Int -> Int
+applyThrice f x = f (f (f x))
+
+addTwo :: Int -> Int
+addTwo x = x + 2
+
+addThree :: Int -> Int
+addThree x = x + 3
+
+addFour :: Int -> Int
+addFour x = x + 4
+
 addMaker :: Int -> (Int -> Int)
-addMaker x = (\y -> x + y)
+addMaker x = (\y -> y + x)
+
+
+subtractMaker :: Int -> (Int -> Int)
+subtractMaker x = (\y -> x - y)
